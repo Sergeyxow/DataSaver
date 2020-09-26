39fba8d9c772bab918e1e48d3554c35206166522
Sergeyxow <59441462+Sergeyxow@users.noreply.github.com>
2020-09-26 19:12:20 +0300

    Update DataSaver.cs

--- DataSaver.cs
+++ DataSaver.cs
@@ -5,60 +5,57 @@ using System.Linq;
 using System.Net.Mime;
 using UnityEngine;
 
-namespace RingBoy.Common
+public static class DataSaver<T> where T : class, new()
 {
-    public static class DataSaver<T> where T : class, new()
+    public static T Instance
     {
-        public static T Instance
+        get => GetOrCreateInstance();
+        set
         {
-            get => GetOrCreateInstance();
-            set
+            instance = value;
+            Save();
+        }
+    }
+    private static T instance;
+    private static readonly string path = $"{Application.persistentDataPath}/{typeof(T).Name}.json";
+
+    public static void Save()
+    {
+        File.WriteAllText(path, JsonUtility.ToJson(Instance));
+    }
+
+
+    /// <summary>
+    /// Clears the data just in runtime.
+    /// Note: To clear file you need to call Save() method
+    /// </summary>
+    public static void Clear()
+    {
+        instance = null;
+    }
+
+    private static T GetOrCreateInstance()
+    {
+        if (instance == null)
+            instance = LoadOrCreate();
+        return instance;
+    }
+
+    private static T LoadOrCreate()
+    {
+        if (File.Exists(path))
+        {
+            try
             {
-                instance = value;
-                Save();
+                var data = JsonUtility.FromJson<T>(File.ReadAllText(path));
+                return data;
+            }
+            catch
+            {
+                return new T();
             }
         }
-        private static T instance;
-        private static readonly string path = $"{Application.persistentDataPath}/{typeof(T).Name}.json";
 
-        public static void Save()
-        {
-            File.WriteAllText(path, JsonUtility.ToJson(Instance));
-        }
-
-
-        /// <summary>
-        /// Clears the data just in runtime.
-        /// Note: To clear file you need to call Save() method
-        /// </summary>
-        public static void Clear()
-        {
-            instance = null;
-        }
-
-        private static T GetOrCreateInstance()
-        {
-            if (instance == null)
-                instance = LoadOrCreate();
-            return instance;
-        }
-
-        private static T LoadOrCreate()
-        {
-            if (File.Exists(path))
-            {
-                try
-                {
-                    var data = JsonUtility.FromJson<T>(File.ReadAllText(path));
-                    return data;
-                }
-                catch
-                {
-                    return new T();
-                }
-            }
-            
-            return new T();
-        }
+        return new T();
     }
 }
